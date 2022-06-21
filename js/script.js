940e69830ac98f6259a6daf511b70c3f28837d07
Laura Bigoni <laurabigoni.92@gmail.com>
2022-06-21 18:03:25 +0200

    rename img folder + function printnotes

--- js/script.js
+++ js/script.js
@@ -10,9 +10,9 @@ $(document).ready(function () {
 
 function getAllNotes() {
   // Ajax endpoint + api Ajax call
-  const endPoint = 'https://62b1dd0920cad3685c84c611.mockapi.io/:endpoint';
+  const endPoint = 'https://62b1dd0920cad3685c84c611.mockapi.io/notes?sortBy=updatedAt&order=desc';
   $.ajax({
-    type: "GET",
+    method: "GET",
     url: endPoint,
     success: function (data) {
       if (data.length > 0) {
@@ -27,5 +27,60 @@ function getAllNotes() {
 
 // Stampo i risultati della chiamata Ajax
 function printListNotes(data) {
-  console.log(data);
+  // Uso il template di handlebars
+  const listYearsContainer = $('.notes-menu .notes-menu__main .notes__container');
+  const src = $('#note-year').html();
+  const template = Handlebars.compile(src);
+
+  // Prendo gli anni dalle note
+  const years = [];
+
+  data.forEach((note) => {
+    const year = dayjs(note.updatedAt).format('YYYY');
+
+    // Se non c'è già l'anno trovato lo pusho nell'array
+    if (!years.includes(year)) {
+      years.push(year);
+    }
+  });
+  // console.log(years, data);
+
+  // Stampo gli anni dalle note
+  years.forEach((year) => {
+    const html = template({ year });
+    listYearsContainer.append(html);
+
+    const notesFiltered = data.filter(function (note) {
+      return dayjs(note.updatedAt).format('YYYY') === year;
+    });
+
+    printNotes(notesFiltered, year);
+  });
+}
+
+// Stampo le note
+function printNotes(data, year) {
+  const listNotesContainer = $('.year' + year + ' .notes-menu__list');
+
+  const src = $('#note-list-template').html();
+  const template = Handlebars.compile(src);
+
+  data.forEach((note) => {
+    const fakeDiv = $('<div></div>').append(note.text);
+    const text = fakeDiv.text().slice(0, 30) + '...';
+
+    // console.log(text);
+
+    // Creo un oggetto per il template
+    const context = {
+      id: note.id,
+      title: note.title,
+      text: text,
+      updatedAt: dayjs(note.updatedAt).format('DD MMMM YYYY'),
+    };
+
+    // Infine lo mostro in pagine
+    const html = template(context);
+    listNotesContainer.append(html);
+  });
 }
